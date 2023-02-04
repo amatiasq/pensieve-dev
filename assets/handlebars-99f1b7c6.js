9b418e141aa901de5ec4706b7722e6eb0160783a
amatiasq <amatiasq@users.noreply.github.com>
2023-02-04 03:53:05 +0000

    deploy: 980fdb8962fa4b0d7ecddd643130b72ca2ec101f

--- assets/handlebars-99f1b7c6.js
+++ assets/handlebars-99f1b7c6.js
@@ -1,4 +1,4 @@
-import{m as i}from"./index-98c4fa1e.js";import"https://unpkg.com/isomorphic-git@beta/http/web/index.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./index-1f17a3c6.js";import"https://unpkg.com/isomorphic-git/http/web/index.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
  * Released under the MIT license
